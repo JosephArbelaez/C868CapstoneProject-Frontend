5a57d980a2c63895dc9b5f97e5a9be7a3a144172
JosephArbelaez <josepharbelaez@gmail.com>
2021-05-16 23:09:20 -0400

    Charge Card

--- src/components/Page/UserAccount.js
+++ src/components/Page/UserAccount.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import BookCatalog from '../Catalog/BookCatalog';
 import BookCollection from '../User/BookCollection';
+import ChargeCard from '../User/ChargeCard';
 import UserCard from '../User/UserCard';
 
 class UserAccount extends Component {
@@ -15,7 +16,9 @@ class UserAccount extends Component {
             bookCollection: [],
             dropzoneActive: false,
             file: [],
-            value: '' 
+            charges: [],
+            chargeSum: 0,
+            value: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -25,29 +28,53 @@ class UserAccount extends Component {
 
     componentDidMount() {
         axios.get(`http://localhost:8080/api/v1/book`)
-        .then((res) => {
-            var temp = [];
+            .then((res) => {
+                var temp = [];
 
-            for (var i = 0; i < res.data.length; i++) {
-                res.data[i].title = (res.data[i].title.charAt(0).toUpperCase() + res.data[i].title.slice(1));
-                if(res.data[i].person){
-                if(res.data[i].person.userID == this.props.userID){
-                    temp.push(res.data[i]);
+                for (var i = 0; i < res.data.length; i++) {
+                    res.data[i].title = (res.data[i].title.charAt(0).toUpperCase() + res.data[i].title.slice(1));
+                    if (res.data[i].person) {
+                        if (res.data[i].person.userID == this.props.userID) {
+                            temp.push(res.data[i]);
+                        }
+                    }
                 }
-            }
-            }
 
-            this.setState({
-                books : res.data,
-                bookResults: res.data,
-                bookCollection: temp
-            });
-        })
+                this.setState({
+                    books: res.data,
+                    bookResults: res.data,
+                    bookCollection: temp
+                });
+            })
+
+        axios.get(`http://localhost:8080/api/v1/charge`)
+            .then((res) => {
+                var temp = [];
+                var total = [];
+                for (var i = 0; i < res.data.length; i++) {
+                    if (res.data[i].patron) {
+                        if (res.data[i].patron.userID == this.props.userID) {
+                            temp.push(res.data[i]);
+                            total.push(res.data[i].price);
+                        }
+                    }
+                }
+
+                var sum = total.reduce(function(a, b){
+                    return a + b;
+                }, 0);
+                
+                this.setState({
+                    charges: temp,
+                    chargeSum: sum.toFixed(2)
+                })
+            }
+            )
     }
 
     changePhoto() {
         this.setState({
-            dropzoneActive:true
+            dropzoneActive: true
         })
     }
 
@@ -67,30 +94,33 @@ class UserAccount extends Component {
         });
     }
     handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+        this.setState({ value: event.target.value });
+    }
     toPhoto = () => {
         this.props.toPhoto();
     }
 
     render() {
         return (
-            <div className = "userAccountPage">
-                <UserCard 
-                    userID = {this.props.userID}
-                    name = {this.props.name}
-                    cardNumber = {this.props.cardNumber}
-                    dropzoneActive = {this.state.dropzoneActive}
-                    changePhoto = {this.changePhoto}
-                    file = {this.file}
-                    url = {this.props.url}
-                    toPhoto = {this.toPhoto} />
+            <div className="userAccountPage">
+                <div>
+                    <UserCard
+                        userID={this.props.userID}
+                        name={this.props.name}
+                        cardNumber={this.props.cardNumber}
+                        dropzoneActive={this.state.dropzoneActive}
+                        changePhoto={this.changePhoto}
+                        file={this.file}
+                        url={this.props.url}
+                        toPhoto={this.toPhoto} />
+                    <ChargeCard chargeTotal={this.state.chargeSum}/>
+                </div>
                 <form onSubmit={this.searchBooks}>
                     <input type="text" value={this.state.value} onChange={this.handleChange} />
                     <input type="submit" value="Submit" />
                 </form>
-                <BookCatalog books={this.state.bookResults} searchBooks={this.searchBooks}/>
-                <BookCollection books={this.state.bookCollection}/>
+                <BookCatalog books={this.state.bookResults} searchBooks={this.searchBooks} />
+                <BookCollection books={this.state.bookCollection} />
             </div>
         );
     }
--- src/components/User/BookCollection.js
+++ src/components/User/BookCollection.js
@@ -1,4 +1,6 @@
 import React, { Component} from 'react';
+import { jsPDF } from "jspdf";
+import 'jspdf-autotable'
 
 const Row = ({ isbn, title, author, price, checkoutDate}) => (
   <div className="tableRow">
@@ -21,13 +23,42 @@ class BookTableChecked extends Component {
       price: 0.0,
       checkoutDate: ""
     }
+
+    this.exportBookCollectionPDF = this.exportBookCollectionPDF.bind(this);
   }
+
+  exportBookCollectionPDF = () => {
+    let unit = "pt";
+    let size = "A4";
+    let orientation = "portrait";
+
+    let marginLeft = 40;
+    let doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    let title = "My Awesome Report";
+    let headers = [["isbn", "title", "author", "price", "Checkout Date"]];
+
+    let bookCollection = this.props.books.map(elt => [elt.isbn, elt.title, elt.author, elt.price, elt.checkoutDate]);
+
+    let content = {
+        startY: 50,
+        head: headers,
+        body: bookCollection
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("report.pdf")
+}
 
   render() {
     const rows = this.props.books.map((rowData) => <Row {...rowData} />);
 
     return (
       <div className="table">
+        <button onClick={this.exportBookCollectionPDF}>Generate Checked Report</button>
         <div className="tableHeader">
           <div>ISBN</div>
           <div>Title</div>
